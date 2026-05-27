54ee53350e4c8bb5c7af18f6fb2bd6d9f1ff6697
Mark100 <gameturbo888@gmail.com>
2026-05-27 19:40:56 +0300

    lession-2-e

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
+
 import sys
+from openai import OpenAI
 from os import getenv
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
@@ -11,31 +13,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = getenv("TOKEN")
-
+client = OpenAI()
 dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
+
    
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
+    if message.text:
+        response = client.responses.create(
+            model="gpt-5.5",
+            input=message.text)
+        await message.answer(response.output_text)
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
+    else:
         await message.answer("Nice try!")
+
+    
+        
+        
 
 
 async def main() -> None:
