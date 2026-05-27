9294d95ca8b9920b13476df2ac766c84ade072d4
Tamerlan Shibzuhov <shibzuhovtamik@gmail.com>
2026-05-27 18:58:15 +0300

    openai.chatbot

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from openai import OpenAI
 import asyncio
 import logging
 import sys
@@ -11,11 +12,11 @@ from aiogram.types import Message
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = getenv("TOKEN")
-
+OPENAI_TOKEN = getenv("OPENAI_TOKEN")
 # All handlers should be attached to the Router (or Dispatcher)
 
 dp = Dispatcher()
-
+client = OpenAI(api_key=OPENAI_TOKEN)
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
@@ -32,17 +33,15 @@ async def command_start_handler(message: Message) -> None:
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+    if message.text:
+        response = client.responses.create(
+        model="gpt-5.5",
+        input=message.text,)
+
+        await message.answer(response.output_text)
+    else:
+        await message.answer("Ну ты конечно красавчик, я не отрицаю, но давай мне текст свой сюда!")
 
 
 async def main() -> None:
