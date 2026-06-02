b2ffb1500d3ae9d7d9895f96e5a4fa2508a05fe9
Mark100 <gameturbo888@gmail.com>
2026-06-02 19:28:46 +0300

    fix_lession-2-e

--- main.py
+++ main.py
@@ -1,50 +1,6 @@
 import asyncio
 import logging
-<<<<<<< HEAD
-import sys
-from os import getenv
-from aiogram import Bot, Dispatcher, html
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from dotenv import load_dotenv
-load_dotenv()
 
-TOKEN = getenv("TOKEN")
-
-dp = Dispatcher()
-
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-   
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
-
-
-async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    # And the run events dispatching
-=======
 import os
 import sys
 from os import getenv
@@ -151,7 +107,7 @@ async def unknown_handler(message: Message) -> None:
 
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
->>>>>>> origin/lession-2-e
+
     await dp.start_polling(bot)
 
 
